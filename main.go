abd28fb31f32a21691eaf50c5cc8e9564871072c
thiagotfsilva <thiagophsilva@hotmail.com>
2024-11-09 23:22:43 -0300

    Adicionando variaveis de ambiente

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/hex"
 	"fmt"
 	"log"
 	"net/http"
@@ -8,12 +9,21 @@ import (
 	"web-app-go/src/cookies"
 	"web-app-go/src/router"
 	"web-app-go/src/utils"
+
+	"github.com/gorilla/securecookie"
 )
+
+func init() {
+	hashKey := hex.EncodeToString(securecookie.GenerateRandomKey(16))
+	fmt.Println(hashKey)
+
+	blockKey := hex.EncodeToString(securecookie.GenerateRandomKey(16))
+	fmt.Println(blockKey)
+}
 
 func main() {
 	config.Load()
 	cookies.Config()
-	fmt.Println(config.Port)
 	utils.LoadTemplates()
 	r := router.Router()
 	fmt.Printf("Rodando na porta %d\n", config.Port)
